e5f5c4e3c195f8f00cc2e354d5dd28dddea92d64
Sean <sevensidedsilver@gmail.com>
2017-05-13 19:22:38 -0600

    small additions to new tweet

--- js/javascript.js
+++ js/javascript.js
@@ -13,12 +13,10 @@ $(document).ready(function(){
 // we need to find the actual length of the characters
 
 $('#gutter-Tweet').focus(function(){
-  // var textCount = $('textarea').val().length
-  // console.log(textCount);
+
 
 var keyListener = function(){
   let gutterCharaCount =  140 - $(this).val().length;
-  console.log(gutterCharaCount);
   $("#char-count").html(gutterCharaCount)
     if (gutterCharaCount <= 10){
     $("#char-count").css('color', 'red')
@@ -26,6 +24,8 @@ var keyListener = function(){
     $("#char-count").css('color', '#999')
   } if (gutterCharaCount < 0){
     $('#tweet-submit').prop("disabled", true);
+  } if (gutterCharaCount >= 0) {
+    $('#tweet-submit').prop("disabled", false);
   }
 }
 
@@ -34,7 +34,44 @@ var keyListener = function(){
   $('#gutter-Tweet').keydown(keyListener)
 })
 
+//the following blocks create a new tweet in the feed
+// a tweet should only be created if there is something typed in the textbox
 
+$('#tweet-submit').click(function(){
+  let counted = $('#gutter-Tweet').val().length;
+  if  (counted > 0) {
+    //prepend the stream with a new tweet
+
+    //this variable will contain all of our retweets and response action Elements
+    let actions = $("<div/>")
+                  .addClass("tweet-actions")
+                  .html('<ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul>')
+
+
+
+
+
+    //this is the text from the textbox
+    let message = $('#gutter-Tweet').val()
+
+
+    let TweetContent = $("<div/>")
+                    .addClass("content")
+                    .html("<img class='avatar' src='img/alagoon.jpg'><strong class='fullname'>Your Name Here</strong><span class='username'>@mybff</span><p class='tweet-text' >"+message+"</p>")
+                    .append(actions)
+
+    let newTweet = $("<div/>")
+                    .addClass("tweet")
+                    .html(TweetContent)
+
+
+
+
+    $('#stream').prepend(newTweet);
+
+
+}
+});
 
 
 
